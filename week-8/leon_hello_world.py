5e950ad1ce013498e484c99b73386e3ab51e5f7d
Wendy Leon <pixelmamma@gmail.com>
2020-12-03 14:32:34 -0500

    Adding 8.1

---
+++ week-8/leon_hello_world.py
@@ -0,0 +1,10 @@
+# ===========================================
+# Title:  Hello World 
+#  Author: Professor Krasso
+#  Date:   3 December 2021
+#  Modified by: Wendy Leon
+# Description: Python file / Anaconda Nv
+# ==========================================
+# 
+
+print('Hello World')
