0114cfee8ea149442fa4ad3e4fffaa442faaee6a
Wendy Leon <pixelmamma@gmail.com>
2020-12-03 23:03:43 -0500

    Adding8.3

---
+++ week-8/leon_calculator.py
@@ -0,0 +1,22 @@
+# ========================================
+# Title: Calculator
+# Author: Richard Krasso
+# Modified by: Wendy Leon
+# Date:  12/3/20
+# Description: Assignment 8.3 
+# =========================================
+
+def add(num1, num2):
+    return num1 + num2
+
+def subtract(num1, num2):
+    return num1 - num2
+
+def divide(num1, num2):
+    return num1 / num2
+
+print(add(4, 2))
+
+print(subtract(6, 2))
+
+print(divide(4, 2))
