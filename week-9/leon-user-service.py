b9a959765a159ac83366250b2877b49a3c6dba88
Wendy Leon <pixelmamma@gmail.com>
2020-12-08 20:42:14 -0500

    edits 9.2

--- week-9/leon-user-service.py
+++ week-9/leon-user-service.py
@@ -1,10 +1,10 @@
-# ========================================
+# =============================================
 # Title: Exercise 9.2 
 # Author: Richard Krasso
 # Modified by: Wendy Leon
 # Date:  12/8/20
-# Description: Querying and Creating Documents
-# =========================================
+# Description: Querying and Creating a Document
+# ==============================================
 
 from pymongo import MongoClient
 
@@ -13,9 +13,9 @@ import pprint
 import datetime
 
 client=MongoClient('localhost', 27017)
-
+#use web 335
 db=client.web335
-
+#create user
 user={
     "first_name": "Wen",
     "last_name": "D",
@@ -23,9 +23,9 @@ user={
     "employee_id": "001",
     "date_created": datetime.datetime.utcnow()
 }
-
+#insert user
 user_id = db.users.insert_one(user).inserted_id
-
+#print user id
 print(user_id)
-
-pprint.pprint(db.users.find_one({"employee_id": "0000001"}))
+#print user w/ id 001
+pprint.pprint(db.users.find_one({"employee_id": "001"}))
